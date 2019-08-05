3d0e7de5d9dcc5177bca6f7fc3ab734bb9d7f013
Alice <alice@bucketplace.net>
2019-08-05 14:15:29 +0900

    fix: properties null 예외처리

--- lib/omniauth-kakao/version.rb
+++ lib/omniauth-kakao/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Kakao
-    VERSION = "0.1.5"
+    VERSION = "0.1.6"
   end
 end
--- lib/omniauth/strategies/kakao.rb
+++ lib/omniauth/strategies/kakao.rb
@@ -63,7 +63,7 @@ module OmniAuth
       end
 
       def raw_properties
-        @raw_properties ||= raw_info['properties']
+        @raw_properties ||= raw_info.fetch('properties', {})
       end
     end
   end
